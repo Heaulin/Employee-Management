c074e71e7900eaa267a68c2e28227538f1999eed
Samantha Mack <heaulin@gmail.com>
2021-10-08 00:28:07 -0700

    updated schema sql

---
+++ sql/schema.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE roles (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
+
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) UNIQUE NOT NULL,
+  last_name VARCHAR(30) UNIQUE NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT 
+);
