884d54488d79d95db3218c1451e8101ac6be93cc
Samantha Mack <heaulin@gmail.com>
2021-10-08 01:15:05 -0700

    updated schema sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department(
+CREATE TABLE departments(
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(30) UNIQUE NOT NULL
 );
@@ -12,7 +12,8 @@ CREATE TABLE roles (
   id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 
@@ -21,5 +22,19 @@ CREATE TABLE employee (
   first_name VARCHAR(30) UNIQUE NOT NULL,
   last_name VARCHAR(30) UNIQUE NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT 
+  manager_id INT, 
+  FOREIGN KEY (role_id) REFERENCES departments(id)
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
+
+SELECT employee.id, 
+employee.first_name, 
+employee.last_name, 
+role.title, 
+role.salary, 
+department.name 
+AS 'department', CONCAT(manager.first_name, '', manager.last_name) AS 'manager'
+FROM employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id
+LEFT JOIN employee manager ON manager.id = employee.manager_id
